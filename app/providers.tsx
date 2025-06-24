aee5cad3932dc3412b923a1ede57cf53aecff3a0
shadyan <vladimir.shadyan+git@gmail.com>
2025-06-24 20:18:07 +0400

    Update PostHog options to always track events

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,7 +8,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only',
+      person_profiles: 'always',
       defaults: '2025-05-24'
     })
   }, [])
